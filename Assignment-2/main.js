428e32cb35f60412aa60d8859c78c447397581b8
Diego Rivera <91293110+Diegoriv-era@users.noreply.github.com>
2023-02-13 17:26:47 -0800

    Add files via upload

---
+++ Assignment-2/main.js
@@ -0,0 +1,25 @@
+var gl;
+var cone;
+
+function init() {
+	var canvas = document.getElementById("webgl-canvas");
+	gl = canvas.getContext("webgl2");
+	gl.clearColor(0, 0, 0, 1);
+	cone = new Cone(gl, 20);
+	render();
+
+
+};
+
+
+
+
+
+
+function render() {
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	cone.render();
+	//window.onload = init;
+};
+
+window.onload = init;
